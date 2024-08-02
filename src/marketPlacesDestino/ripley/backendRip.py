7e9533d932155ca0075112290dc16d33bc129ef5
Ernesto <rising.resn@gmail.com>
2024-08-01 23:00:37 -0500

    all ripley paths extracted

--- src/marketPlacesDestino/ripley/backendRip.py
+++ src/marketPlacesDestino/ripley/backendRip.py
@@ -4,21 +4,21 @@ import requests
 
 cookies = {
     '_ga': 'GA1.3.397895214.1721248399',
-    '_gid': 'GA1.3.1227843975.1722365107',
     '_hjSessionUser_1266356': 'eyJpZCI6IjMyZjFhMmJjLTBjYzUtNWMwZS04NzFlLTU4ZDJlM2E1MjExNiIsImNyZWF0ZWQiOjE3MjIzNjU1OTY2MTIsImV4aXN0aW5nIjp0cnVlfQ==',
     '_ga': 'GA1.1.397895214.1721248399',
     'XSRF-TOKEN': '82c723ea-eae1-45f9-b130-562cb3bd13e6',
-    '_hjSession_1266356': 'eyJpZCI6IjEzZjRmOWY5LTBlNjgtNGZhNy05YWU5LTEzODFlOWIxNDQ5ZiIsImMiOjE3MjIzODcxMDgyMTMsInMiOjEsInIiOjAsInNiIjowLCJzciI6MCwic2UiOjAsImZzIjowLCJzcCI6MX0=',
+    '_gid': 'GA1.3.576861606.1722559628',
+    '_hjSession_1266356': 'eyJpZCI6IjljN2QyNjQ4LWVjYjQtNDYyZS05ZmYyLTgyZWM5M2QwZWNhOSIsImMiOjE3MjI1NjgwMTI2ODMsInMiOjEsInIiOjAsInNiIjowLCJzciI6MCwic2UiOjAsImZzIjowLCJzcCI6MH0=',
     '_gat': '1',
-    'Authorization': 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJta3BpbnRlckB1bmFsdWthLmNvbSIsImF1ZCI6InJpcGxleXBlcnUtcHJvZCIsImF1dGhlbnRpY2F0aW9uLW9yaWdpbiI6Ik1JUkFLTF9TU08iLCJleHRyYSI6eyJ0b3MiOiJ0cnVlIiwiYXV0aDAtaWQtdG9rZW4iOiJleUpoYkdjaU9pSlNVekkxTmlJc0luUjVjQ0k2SWtwWFZDSXNJbXRwWkNJNklrNVVWVEJOVlVwRlVsVk5lVkpFVmtaUk1FVXpUWHBhUjAxVVJrWk9SR3QzVFRCT1JGRnFhelJOZWsxNVVsUkdSVkpGU1ROU1p5SjkuZXlKb2RIUndjem92TDIxcGNtRnJiQzVqYjIwdmNtOXNaWE1pT2xzaVpHOWpkVzFsYm5SaGRHbHZianBTVDB4RlgxTkZURXhGVWlKZExDSm9kSFJ3Y3pvdkwyMXBjbUZyYkM1amIyMHZiM0pwWjJsdUlqb2liV2x5WVd0c1gzTnpieUlzSW1oMGRIQnpPaTh2YldseVlXdHNMbU52YlM5c2IyTmhiR1VpT2lKbGN5SXNJbVZ0WVdsc0lqb2liV3R3YVc1MFpYSkFkVzVoYkhWcllTNWpiMjBpTENKbGJXRnBiRjkyWlhKcFptbGxaQ0k2ZEhKMVpTd2lhWE56SWpvaWFIUjBjSE02THk5c2IyZHBiaTV0YVhKaGEyd3VibVYwTHlJc0ltRjFaQ0k2SWxWT1VFSTBTMkpUZWpFd1drVjRSbmxTYzA1Uk5rcElZa3RDWlZjNU5HNXhJaXdpYVdGMElqb3hOekl5TXpnM01UQTNMQ0psZUhBaU9qRTNNakkwTWpNeE1EY3NJbk4xWWlJNkltRjFkR2d3ZkRZell6Z3dNR0UzWm1ZNE1qSmpabUUyTWpReFltWm1NaUlzSW5OcFpDSTZJa2hNTFZKSE1sZEdOemszZVhKSU5VcHdUR2t5VkZoUFIyTmlWVTV6VFVoVklpd2libTl1WTJVaU9pSlVOVTlDY0ZCVlozZHlNVVpNUVhCcFJuaE1XbVZYYnpoTlEwaGhWakpHTmxSdVRXVnNVR1JTTVdWVkluMC5hWVFtbFRKNjNMeDRwSW9qWWFNWnJlQl9fXzRSSUhrSXc2ckRuZ2hJWV9TRlRXdUI5ZUowV2lCTEduVVdERk9IOFlXVFg3eXRyRUExdGVSdlVIMFlCdUNKRDNoMVI4NGdndHgxTE12aUJQeFMzT0xYaEY5aTd5aFNYMldtY3VQZzZ6X3dFMFJpc00wQmdydmpYMV9EYmpta0hQdTBadkRvNENnYURBZHZ2TXYwS0tObG05RVVZdExHYmdwbVJNOWljZ0E2OFduaTRKc2gxYjZ4Ym10ZE9veHh1cXkyQ0dUQXlSV0NOWGRjeUdkUHUxTWxEZ3hTb0paMnZJZnNyRkFqNFVPTHVWNmJuNTA2QzkzMUZScE00aE9yenhEUHRqVGFqcWE2T3pqU2VwazFSNWo2RG9sMlVsOUtiTnJjam0wTF82c1JLNG96OENRSFNKMnlUYmp5T0EifSwiZXhwIjoxNzIyMzg5MDQ1LCJzaWQiOiJhZGVjODZiYS0wNThhLTRkYTAtODRhZi1mMmY3MWQ0ZDlmNTgifQ.kctnS7-X9v_7D8a9sfXMqu_dCpLy49-QtNC47t-EEfXRHJjLn1ZR56CGyqi1i1mSlq4u8A6sGTeAdMmYDp4UTg',
-    '_ga_P32X25ZF51': 'GS1.3.1722387102.12.1.1722387244.0.0.0',
-    '_ga_8THW8XBF2T': 'GS1.1.1722387109.5.1.1722387245.0.0.0',
+    '_ga_P32X25ZF51': 'GS1.3.1722568006.15.1.1722568080.0.0.0',
+    'Authorization': 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJta3BpbnRlckB1bmFsdWthLmNvbSIsImF1ZCI6InJpcGxleXBlcnUtcHJvZCIsImF1dGhlbnRpY2F0aW9uLW9yaWdpbiI6Ik1JUkFLTF9TU08iLCJleHRyYSI6eyJ0b3MiOiJ0cnVlIiwiYXV0aDAtaWQtdG9rZW4iOiJleUpoYkdjaU9pSlNVekkxTmlJc0luUjVjQ0k2SWtwWFZDSXNJbXRwWkNJNklrNVVWVEJOVlVwRlVsVk5lVkpFVmtaUk1FVXpUWHBhUjAxVVJrWk9SR3QzVFRCT1JGRnFhelJOZWsxNVVsUkdSVkpGU1ROU1p5SjkuZXlKb2RIUndjem92TDIxcGNtRnJiQzVqYjIwdmNtOXNaWE1pT2xzaVpHOWpkVzFsYm5SaGRHbHZianBTVDB4RlgxTkZURXhGVWlKZExDSm9kSFJ3Y3pvdkwyMXBjbUZyYkM1amIyMHZiM0pwWjJsdUlqb2liV2x5WVd0c1gzTnpieUlzSW1oMGRIQnpPaTh2YldseVlXdHNMbU52YlM5c2IyTmhiR1VpT2lKbGN5SXNJbVZ0WVdsc0lqb2liV3R3YVc1MFpYSkFkVzVoYkhWcllTNWpiMjBpTENKbGJXRnBiRjkyWlhKcFptbGxaQ0k2ZEhKMVpTd2lhWE56SWpvaWFIUjBjSE02THk5c2IyZHBiaTV0YVhKaGEyd3VibVYwTHlJc0ltRjFaQ0k2SWxWT1VFSTBTMkpUZWpFd1drVjRSbmxTYzA1Uk5rcElZa3RDWlZjNU5HNXhJaXdpYVdGMElqb3hOekl5TlRZNE1ERXhMQ0psZUhBaU9qRTNNakkyTURRd01URXNJbk4xWWlJNkltRjFkR2d3ZkRZell6Z3dNR0UzWm1ZNE1qSmpabUUyTWpReFltWm1NaUlzSW5OcFpDSTZJa1pxTWpaUFkwMWFiRFZsVW1aamJHbFNNbEozWDA5YWVXVTFaRGxGYkV4Tklpd2libTl1WTJVaU9pSXdZVk0wZFdocFZrbHdRMjVJTVZrdE5FVmtlbTVWZW5Bd2QxcERVMjR0Y0dGSmQyaG1lWEJWV0ZKbkluMC5VM0pONTUtdFFSeVpmWXdzTmZKc1J0dVpoY3V4Vjc2ZklqNl9fbEp1bTZDRk56NXFlMGt3Yl93OHA2RzNpWUVJTkpSdFkxb1IzdmZ1MmZxM2t5SlJxQmVaX2NEU3lPZjVRVWd1WUhnMG1mNFJXRi1mREtfSGtFMDFmdms5eWsyT252OENBN0pKeEJSTW41NHB1T3BzazRITWpydEZ1Zmt0RExKTWotck5BV1Rfai02d1A0RlhmZkZlb0NsNDBwYktIZFA0WjVpN2NDV3FGT0Mya3BPZ2tqNG5aRkcwLU5tblNFTkRYaHZnQm8wOVl2VFdWbC13akdMWE5yT3VZUnlKTmhWRkRKdzExNVVmaDVjeWVLR2JfeGM0Q2xjOUFWNHVVaXhLT2pDaHR5RnRmbF9oSjVXWWRuX1dzV0hTWlgwTndZTF9iWE1mTndHUGk1UFZDUVJ0SEEifSwiZXhwIjoxNzIyNTY5ODgyLCJzaWQiOiI2N2JjMTQ3Yi02MWQ1LTRhYjYtOTY4NC01NDJmMWM4YzNkMTgifQ.fD_lgfF_QyJqVuN5lVX_3XyHljzLyXC0BM3XrBxMO-aYnbz1USIjaRcrBlJaQbNF7IVcsLySW6DCQ07KW2N8tw',
+    '_ga_8THW8XBF2T': 'GS1.1.1722568014.8.1.1722568083.0.0.0',
 }
 
 headers = {
     'accept': 'application/json, text/plain, */*',
     'accept-language': 'es-419,es;q=0.9',
-    # 'cookie': '_ga=GA1.3.397895214.1721248399; _gid=GA1.3.1227843975.1722365107; _hjSessionUser_1266356=eyJpZCI6IjMyZjFhMmJjLTBjYzUtNWMwZS04NzFlLTU4ZDJlM2E1MjExNiIsImNyZWF0ZWQiOjE3MjIzNjU1OTY2MTIsImV4aXN0aW5nIjp0cnVlfQ==; _ga=GA1.1.397895214.1721248399; XSRF-TOKEN=82c723ea-eae1-45f9-b130-562cb3bd13e6; _hjSession_1266356=eyJpZCI6IjY3Y2NlYzQ2LWE3Y2QtNGJkOC04ODIxLTFlZDg3MzhmMGM4ZCIsImMiOjE3MjIzODQ3OTE2NzgsInMiOjEsInIiOjAsInNiIjowLCJzciI6MCwic2UiOjAsImZzIjowLCJzcCI6MH0=; _ga_P32X25ZF51=GS1.3.1722384783.11.1.1722384898.0.0.0; _ga_8THW8XBF2T=GS1.1.1722384793.4.1.1722384899.0.0.0; Authorization=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJta3BpbnRlckB1bmFsdWthLmNvbSIsImF1ZCI6InJpcGxleXBlcnUtcHJvZCIsImF1dGhlbnRpY2F0aW9uLW9yaWdpbiI6Ik1JUkFLTF9TU08iLCJleHRyYSI6eyJ0b3MiOiJ0cnVlIiwiYXV0aDAtaWQtdG9rZW4iOiJleUpoYkdjaU9pSlNVekkxTmlJc0luUjVjQ0k2SWtwWFZDSXNJbXRwWkNJNklrNVVWVEJOVlVwRlVsVk5lVkpFVmtaUk1FVXpUWHBhUjAxVVJrWk9SR3QzVFRCT1JGRnFhelJOZWsxNVVsUkdSVkpGU1ROU1p5SjkuZXlKb2RIUndjem92TDIxcGNtRnJiQzVqYjIwdmNtOXNaWE1pT2xzaVpHOWpkVzFsYm5SaGRHbHZianBTVDB4RlgxTkZURXhGVWlKZExDSm9kSFJ3Y3pvdkwyMXBjbUZyYkM1amIyMHZiM0pwWjJsdUlqb2liV2x5WVd0c1gzTnpieUlzSW1oMGRIQnpPaTh2YldseVlXdHNMbU52YlM5c2IyTmhiR1VpT2lKbGN5SXNJbVZ0WVdsc0lqb2liV3R3YVc1MFpYSkFkVzVoYkhWcllTNWpiMjBpTENKbGJXRnBiRjkyWlhKcFptbGxaQ0k2ZEhKMVpTd2lhWE56SWpvaWFIUjBjSE02THk5c2IyZHBiaTV0YVhKaGEyd3VibVYwTHlJc0ltRjFaQ0k2SWxWT1VFSTBTMkpUZWpFd1drVjRSbmxTYzA1Uk5rcElZa3RDWlZjNU5HNXhJaXdpYVdGMElqb3hOekl5TXpnME56a3dMQ0psZUhBaU9qRTNNakkwTWpBM09UQXNJbk4xWWlJNkltRjFkR2d3ZkRZell6Z3dNR0UzWm1ZNE1qSmpabUUyTWpReFltWm1NaUlzSW5OcFpDSTZJa1I1UlVSRVJFcDVTbkJUU1Zwa1lYUk1TalY2U1ZSbFR6SmxZVE5zZVhOcUlpd2libTl1WTJVaU9pSmpNMGxmWkhsc1lVazNSak4wU3pSd2FYZFZSWFE0WTB0SVIyOUVaWGw1VTBsUmNWTkxUR2hEWDNOTkluMC5Cd3BLUld5ejVsUTBucDNDSTF1YlQ2cEQ2OHc3ZE9BTHlSQUZQYVlZd1JWVHhkQmxmLWp5dnVtQ0xnRVBQbTVzNEVMWXduTGotYU1DRmI4MngxTkRPZi1TNUNkNHlxSnFFRWZmc05IR2s5QkdsZW83ZmhFbllHdUd5elVnZUN5aWM3Q0xLbFFmZzRfTmtMdzJSZVA2anNnWnIxeHJ4UzB6UjdFVFduci14SVZSNmhkcTZIbE4tUDVnTG9GZmkwVHViOHozZnFTajlzeHZ3dHA5eEZ2NGF6Nm0xSkdJRDFzWXFXbDBPbUNmVE1ISGdtSEpMSU56VVVMM25Fbnlqc0hoVnFYdS10VGZrMU1NQU9NT3Raalp2VnU5ODhKc0NKZTlJdmRXTlJGU0RYNnplNjNRVTF0eFNmM2NLY1pPSjNYUXRCcmE4TU13c2tGTzZZWXFESlhSS3cifSwiZXhwIjoxNzIyMzg2NzQyLCJzaWQiOiJiODQxNjI2My0xNDE2LTRkNzEtOGE5Ni1hMmE2MGFmYzkxOTMifQ.IeCghv86rpriCRbo5gg3ax7VBKVioCYjFm3MqAQmZL8jgtCjroGbEVOcYS9Suyf0zw47skbExO-J7Z42OYeGzA',
+    # 'cookie': '_ga=GA1.3.397895214.1721248399; _hjSessionUser_1266356=eyJpZCI6IjMyZjFhMmJjLTBjYzUtNWMwZS04NzFlLTU4ZDJlM2E1MjExNiIsImNyZWF0ZWQiOjE3MjIzNjU1OTY2MTIsImV4aXN0aW5nIjp0cnVlfQ==; _ga=GA1.1.397895214.1721248399; XSRF-TOKEN=82c723ea-eae1-45f9-b130-562cb3bd13e6; _gid=GA1.3.576861606.1722559628; _hjSession_1266356=eyJpZCI6IjljN2QyNjQ4LWVjYjQtNDYyZS05ZmYyLTgyZWM5M2QwZWNhOSIsImMiOjE3MjI1NjgwMTI2ODMsInMiOjEsInIiOjAsInNiIjowLCJzciI6MCwic2UiOjAsImZzIjowLCJzcCI6MH0=; _gat=1; _ga_P32X25ZF51=GS1.3.1722568006.15.1.1722568080.0.0.0; Authorization=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJta3BpbnRlckB1bmFsdWthLmNvbSIsImF1ZCI6InJpcGxleXBlcnUtcHJvZCIsImF1dGhlbnRpY2F0aW9uLW9yaWdpbiI6Ik1JUkFLTF9TU08iLCJleHRyYSI6eyJ0b3MiOiJ0cnVlIiwiYXV0aDAtaWQtdG9rZW4iOiJleUpoYkdjaU9pSlNVekkxTmlJc0luUjVjQ0k2SWtwWFZDSXNJbXRwWkNJNklrNVVWVEJOVlVwRlVsVk5lVkpFVmtaUk1FVXpUWHBhUjAxVVJrWk9SR3QzVFRCT1JGRnFhelJOZWsxNVVsUkdSVkpGU1ROU1p5SjkuZXlKb2RIUndjem92TDIxcGNtRnJiQzVqYjIwdmNtOXNaWE1pT2xzaVpHOWpkVzFsYm5SaGRHbHZianBTVDB4RlgxTkZURXhGVWlKZExDSm9kSFJ3Y3pvdkwyMXBjbUZyYkM1amIyMHZiM0pwWjJsdUlqb2liV2x5WVd0c1gzTnpieUlzSW1oMGRIQnpPaTh2YldseVlXdHNMbU52YlM5c2IyTmhiR1VpT2lKbGN5SXNJbVZ0WVdsc0lqb2liV3R3YVc1MFpYSkFkVzVoYkhWcllTNWpiMjBpTENKbGJXRnBiRjkyWlhKcFptbGxaQ0k2ZEhKMVpTd2lhWE56SWpvaWFIUjBjSE02THk5c2IyZHBiaTV0YVhKaGEyd3VibVYwTHlJc0ltRjFaQ0k2SWxWT1VFSTBTMkpUZWpFd1drVjRSbmxTYzA1Uk5rcElZa3RDWlZjNU5HNXhJaXdpYVdGMElqb3hOekl5TlRZNE1ERXhMQ0psZUhBaU9qRTNNakkyTURRd01URXNJbk4xWWlJNkltRjFkR2d3ZkRZell6Z3dNR0UzWm1ZNE1qSmpabUUyTWpReFltWm1NaUlzSW5OcFpDSTZJa1pxTWpaUFkwMWFiRFZsVW1aamJHbFNNbEozWDA5YWVXVTFaRGxGYkV4Tklpd2libTl1WTJVaU9pSXdZVk0wZFdocFZrbHdRMjVJTVZrdE5FVmtlbTVWZW5Bd2QxcERVMjR0Y0dGSmQyaG1lWEJWV0ZKbkluMC5VM0pONTUtdFFSeVpmWXdzTmZKc1J0dVpoY3V4Vjc2ZklqNl9fbEp1bTZDRk56NXFlMGt3Yl93OHA2RzNpWUVJTkpSdFkxb1IzdmZ1MmZxM2t5SlJxQmVaX2NEU3lPZjVRVWd1WUhnMG1mNFJXRi1mREtfSGtFMDFmdms5eWsyT252OENBN0pKeEJSTW41NHB1T3BzazRITWpydEZ1Zmt0RExKTWotck5BV1Rfai02d1A0RlhmZkZlb0NsNDBwYktIZFA0WjVpN2NDV3FGT0Mya3BPZ2tqNG5aRkcwLU5tblNFTkRYaHZnQm8wOVl2VFdWbC13akdMWE5yT3VZUnlKTmhWRkRKdzExNVVmaDVjeWVLR2JfeGM0Q2xjOUFWNHVVaXhLT2pDaHR5RnRmbF9oSjVXWWRuX1dzV0hTWlgwTndZTF9iWE1mTndHUGk1UFZDUVJ0SEEifSwiZXhwIjoxNzIyNTY5ODgyLCJzaWQiOiI2N2JjMTQ3Yi02MWQ1LTRhYjYtOTY4NC01NDJmMWM4YzNkMTgifQ.fD_lgfF_QyJqVuN5lVX_3XyHljzLyXC0BM3XrBxMO-aYnbz1USIjaRcrBlJaQbNF7IVcsLySW6DCQ07KW2N8tw; _ga_8THW8XBF2T=GS1.1.1722568014.8.1.1722568083.0.0.0',
     'priority': 'u=1, i',
     'referer': 'https://ripleyperu-prod.mirakl.net/mmp/shop/catalog/template/configure?modelType=PRODUCTS_OFFERS',
     'sec-ch-ua': '"Chromium";v="125", "Not.A/Brand";v="24"',
@@ -37,7 +37,6 @@ params = {
     'selectedLocale': 'es_PE',
 }
 
-
 def generate_response(params,cookies,headers):
     
     response = requests.get(
@@ -47,30 +46,70 @@ def generate_response(params,cookies,headers):
         headers=headers,
     )
 
-    print(response)
-    print(response.text)
-    print(response.json())
-
+    #print(response)
+    #print(response.text)
+    #print(response.json())
     responseLoad=response.json()
     return responseLoad
 
+def get_json_categories(category_code:str):
+    
+    category_params = {
+    'hierarchyCode': '',
+    'selectedLocale': 'es_PE',}
+
+    category_params["hierarchyCode"]=category_code
+    category_json= generate_response(category_params,cookies,headers)
+    return category_json
+
 def response_to_json(responseLoad,name:str):
+    print("Saving response to json...")
     with open("DropShippingAuto/src/marketPlacesDestino/ripley/CategoriesTree/"+name+".json","w",encoding="utf-8") as json_file:
         json.dump(responseLoad,json_file,indent=4,ensure_ascii=False)
 
+def get_children(child):
+    if child["children"]==True:
+        print(child["code"]+"has children")
+        childResponse=get_json_categories(child["code"])
+        child["child"]=childResponse
+        for child in childResponse:
+            get_children(child)
+    else:
+        print(child["code"]+"has no children")
+        child["child"]=[]    
+
+def get_children_w_path(parent):
+    if parent["children"]==True:
+        print(parent["code"]+"has children")
+        childResponse=get_json_categories(parent["code"])
+        parent["child"]=childResponse
+        for child in parent["child"]:
+            child["path"]=copy.deepcopy(parent["path"])
+            child["path"].append(child["label"])
+            get_children_w_path(child)
+    else:
+        print(parent["code"]+"has no children")
+        
+    
+
+
+def get_categories_tree(root_categories_dict):
+    for root_cat in root_categories_dict:
+        get_children_w_path(root_cat)
 
 if __name__ == "__main__":
-    responseLoad=generate_response(params,cookies,headers)
-    response_to_json(responseLoad,"root_categories")
-    print("Done")
-    params_children=copy.deepcopy(params)
-    for child in responseLoad:
-        if child["children"]==True:
-            print(child["code"]+"has children")
-            params_children["hierarchyCode"]=child["code"]
-            childResponse=generate_response(params_children,cookies,headers)
-            response_to_json(childResponse,child["code"])
-        else:
-            print(child["code"]+"has no children")
+    #responseLoad=generate_response(params,cookies,headers)
+    #response_to_json(responseLoad,"root_categories")
+    file_path="DropShippingAuto/src/marketPlacesDestino/ripley/CategoriesTree/root_categories.json"
+    with open(file_path, 'r', encoding='utf-8') as file:
+        root_categories_dict = json.load(file)
+    print(root_categories_dict)
 
-    print("Done")
+    #create path key for each root category
+    for root_cat in root_categories_dict:
+        root_cat["path"]=[root_cat["label"]]
+    print(root_categories_dict)
+
+
+    get_categories_tree(root_categories_dict)
+    response_to_json(root_categories_dict,"CategoryTreeWithPaths")
